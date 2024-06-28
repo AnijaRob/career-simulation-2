79f9e13e5dbc6bd1e8a8422f2fd8d3c255b9a4d6
Anija-Khallil Robinson <nastynij@Anija-Khallils-iMac.local>
2024-06-28 14:34:58 -0400

    ain

---
+++ gp.js
@@ -0,0 +1,29 @@
+
+//math probelm equating to the first number in the vault passcode
+const firstNumber=5*2
+//math problem equating to the second number in the vault passcode
+const secondNumber=2*20
+//math problem equating to third number in the vault passcode
+const thirdNumber=2*20-1
+//tell user they have recieved a message to open the vault
+
+
+//line combining all three numbers to make a passcode
+const passcode= `${firstNumber} ${secondNumber} ${thirdNumber}`;
+
+alert ("You have received this message because you have been chosen to open an important vault. Here is the secret combination: 10,20,39")
+
+
+
+console.log(passcode)
+console.log(secondNumber)
+console.log(thirdNumber)
+
+
+
+
+
+
+
+
+
