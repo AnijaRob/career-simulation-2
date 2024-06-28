4c74020a1c51ec4d500a48dc9a5d261bd140ecb8
Anija-Khallil Robinson <nastynij@Anija-Khallils-iMac.local>
2024-06-28 14:37:07 -0400

    gp.js

--- gp.js
+++ gp.js
@@ -5,12 +5,12 @@ const firstNumber=5*2
 const secondNumber=2*20
 //math problem equating to third number in the vault passcode
 const thirdNumber=2*20-1
-//tell user they have recieved a message to open the vault
+
 
 
 //line combining all three numbers to make a passcode
 const passcode= `${firstNumber} ${secondNumber} ${thirdNumber}`;
-
+//tell user they have recieved a message to open the vault
 alert ("You have received this message because you have been chosen to open an important vault. Here is the secret combination: 10,20,39")
 
 
